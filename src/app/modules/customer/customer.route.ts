18bcc7a945e632ee3944a1f1a1673f2bc71e5226
parvez13913 <parvezz13913@gmail.com>
2024-08-11 21:22:21 +0600

    [DONE] Customer route validation added

--- src/app/modules/customer/customer.route.ts
+++ src/app/modules/customer/customer.route.ts
@@ -1,15 +1,52 @@
 import express from "express";
 import { CustomerController } from "./customer.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { CustomerValidation } from "./customer.validation";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
 
 
 
 const router = express.Router();
 
 
-router.get('/:id', CustomerController.getSingleCustomer);
-router.get('/', CustomerController.getAllCustomers);
-router.patch('/:id', CustomerController.updateCustomer);
-router.delete('/:id', CustomerController.deleteCustomer);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.CUSTOMER
+  ),
+  CustomerController.getSingleCustomer
+);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  CustomerController.getAllCustomers
+);
+
+router.patch(
+  '/:id',
+  validateRequest(CustomerValidation.updateCustomerZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.CUSTOMER
+  ),
+  CustomerController.updateCustomer
+);
+
+router.delete(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  CustomerController.deleteCustomer
+);
 
 
 
