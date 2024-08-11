381d3db5550d4c7ea9bf5de9c9b9dd4b76af83cd
parvez13913 <parvezz13913@gmail.com>
2024-08-11 21:28:47 +0600

    [DONE] Seller route validation added

--- src/app/modules/seller/seller.route.ts
+++ src/app/modules/seller/seller.route.ts
@@ -1,16 +1,51 @@
 import express from "express";
 import { SellerController } from "./seller.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { SellerValidation } from "./seller.validation";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
 
 
 const router = express.Router();
 
-router.get('/:id', SellerController.getSingleSeller);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  SellerController.getSingleSeller
+);
 
-router.get('/', SellerController.getAllSellers);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  SellerController.getAllSellers
+);
 
-router.patch('/:id', SellerController.updateSeller);
+router.patch(
+  '/:id',
+  validateRequest(SellerValidation.updateSellerZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  SellerController.updateSeller
+);
 
-router.delete('/:id', SellerController.deleteSeller);
+router.delete(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  SellerController.deleteSeller
+);
 
 
 
