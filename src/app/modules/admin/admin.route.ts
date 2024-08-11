525d02a9e94cda53760e49a188e755aa30998a01
parvez13913 <parvezz13913@gmail.com>
2024-08-11 21:32:37 +0600

    [DONE] Admin route validation added

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,13 +1,49 @@
+import { AdminValidation } from './admin.validation';
 import express from "express";
 import { AdminController } from "./admin.controller";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
+import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
 
-router.get('/:id', AdminController.getSingleAdmin);
-router.get('/', AdminController.getAllAdmins);
-router.patch('/:id', AdminController.updateAdmin);
-router.delete('/:id', AdminController.deleteAdmin);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  AdminController.getSingleAdmin
+);
+
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  AdminController.getAllAdmins
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AdminValidation.updateAdminZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  AdminController.updateAdmin
+);
+
+router.delete(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  AdminController.deleteAdmin
+);
 
 
 
