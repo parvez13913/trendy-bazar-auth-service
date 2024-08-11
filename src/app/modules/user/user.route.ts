6d8cbdbefc609d32c3a5c7bf02c27461842f987d
parvez13913 <parvezz13913@gmail.com>
2024-08-11 22:23:54 +0600

    [DONE] User validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,17 +1,73 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { UserValidation } from "./user.validation";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
 
 
 
 const router = express.Router();
 
 
-router.get("/:id", UserController.getSingleUser);
-router.get("/", UserController.getAllUsers);
-router.post("/createCustomer", UserController.createCustomer);
-router.post("/createSeller", UserController.createSeller);
-router.post("/createAdmin", UserController.createAdmin);
-router.delete("/:id", UserController.deleteUser);
+router.get(
+  "/:id",
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  UserController.getSingleUser
+);
+
+router.get(
+  "/",
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  UserController.getAllUsers
+);
+
+router.post(
+  "/createCustomer",
+  validateRequest(UserValidation.createUserZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.CUSTOMER
+  ),
+  UserController.createCustomer
+);
+
+router.post(
+  "/createSeller",
+  validateRequest(UserValidation.createUserZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  UserController.createSeller
+);
+
+router.post(
+  "/createAdmin",
+  validateRequest(UserValidation.createUserZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  UserController.createAdmin
+);
+
+router.delete(
+  "/:id",
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+  ),
+  UserController.deleteUser
+);
 
 
 
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { bloodGroup, gender } from "../../../enums/commonEnums";
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    name: z.object({
+      firstName: z.string({
+        required_error: 'First name is required',
+      }),
+      middleName: z.string().optional(),
+      lastName: z.string({
+        required_error: 'Last name is required',
+      })
+    }),
+    gender: z.enum([...gender] as [string, ...string[]], { required_error: 'Gender is required', }),
+    dateOfBirth: z.string({
+      required_error: 'Date of birth is required',
+    }),
+    email: z.string({
+      required_error: 'Email is required',
+    }).email(),
+    contactNo: z.string({
+      required_error: 'Contact no is required',
+    }),
+    emergencyContactNo: z.string().optional(),
+    bloodGroup: z.enum([...bloodGroup] as [string, ...string[]]).optional(),
+    presentAddress: z.string({
+      required_error: 'Present address is required',
+    }),
+    permanentAddress: z.string({
+      required_error: 'Permanent address is required',
+    }),
+    profileImage: z.string().optional(),
+  }),
+});
+
+
+
+export const UserValidation = {
+  createUserZodSchema,
+};
