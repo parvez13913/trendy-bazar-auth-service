be667ea2205f34f629f5cc54f894848829e5fa7b
parvez13913 <parvezz13913@gmail.com>
2024-08-14 16:58:56 +0600

    [DONE] Brand validation

--- src/app/modules/brand/brand.route.ts
+++ src/app/modules/brand/brand.route.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { BrandController } from "./brand.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { BrandValidation } from "./brand.validation";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enum/user";
 
 const router = express.Router();
 
@@ -7,11 +11,37 @@ router.get('/:id', BrandController.getSingleBrand);
 
 router.get('/', BrandController.getAllBrands);
 
-router.post('/createBrand', BrandController.createBrand);
+router.post(
+  '/createBrand',
+  validateRequest(BrandValidation.createBrandZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  BrandController.createBrand
+);
 
-router.patch('/:id', BrandController.updateBrand);
+router.patch(
+  '/:id',
+  validateRequest(BrandValidation.createBrandZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  BrandController.updateBrand
+);
 
-router.delete('/:id', BrandController.deleteBrand);
+router.delete(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SELLER
+  ),
+  BrandController.deleteBrand
+);
 
 
 
